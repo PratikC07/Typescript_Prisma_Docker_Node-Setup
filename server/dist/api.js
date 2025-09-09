c99ca58117c719a2265bd82345bbb00615b605aa
PratikC07 <prateekchitti@gmail.com>
2025-09-09 07:25:59 +0530

    updates

--- server/dist/api.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authRouter from "./modules/auth/auth.route.js";
-// Import other routers as you create them
-// import pollRouter from "./polls/poll.route.js";
-const router = Router();
-router.use("/auth", authRouter);
-// router.use("/polls", pollRouter);
-export default router;
